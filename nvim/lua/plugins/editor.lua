5ffb7e85938bc8d68d4b25a84d5ad17179bd64fa
hatredholder <rattlingserpent@gmail.com>
2023-05-06 13:32:02 +0300

    updated surround config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -455,10 +455,9 @@ return {
   -- Surround - Add/change/delete 󱓼 surrounding 󱤎 delimiter pairs with ease
   {
     "kylechui/nvim-surround",
-    version = "", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup()
+      require("nvim-surround").setup()
     end
   },
 
